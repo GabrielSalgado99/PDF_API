ff075ca78c674f922b2c79e47311ba4a1db0e7c1
Gabriel Salgado <gabriel.salgado@academico.ifs.edu.br>
2021-10-26 20:04:33 -0300

    Remoção da pasta Uploads

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const express = require(`express`);
 
 const app = express();
 app.set(`view engine`, `ejs`);
-app.set("views", "./src/views")
+app.set("views", "./src/views");
 app.use(express.json());
 
-app.use("/", require("./routes/index"))
+app.use("/", require("./routes/index"));
 //require("./routes/index")(app);
 
 //Subindo o servidor
